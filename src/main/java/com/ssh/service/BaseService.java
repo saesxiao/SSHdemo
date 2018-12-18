a22adbcfd2fdda1c91253ddb5c77330ca4b79a26
saesxiao <x8y8y8@163.com>
2018-12-18 11:11:48 +0800

    recive

---
+++ src/main/java/com/ssh/service/BaseService.java
@@ -0,0 +1,12 @@
+package com.ssh.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BaseService {
+
+	/**
+	 * 日志对象
+	 */
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+}
